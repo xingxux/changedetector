e8fb95ad0080ad68ef7a0f369c610d42d07c25e0
Xing Xu <109101546+xingxux@users.noreply.github.com>
2022-07-14 16:25:34 -0700

    Update changedetector.py

--- changedetector.py
+++ changedetector.py
@@ -1,4 +1,6 @@
-f = open("history/last", "r")
-v = int(f.read())
-print(v+1)
+version_file = open("history/last", "r")
+last = int(version_file.read())
+
+last_file = open("history/" + last, "r")
+print(last_file)
 
