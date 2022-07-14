c3cbb7603f2ca6968be79c9e34bb6f02155e7276
Xing Xu <109101546+xingxux@users.noreply.github.com>
2022-07-14 16:31:53 -0700

    Update changedetector.py

--- changedetector.py
+++ changedetector.py
@@ -3,10 +3,10 @@ last_file = open("history/" + str(last_version), "r")
 print(last_file.read())
 
 new_file = open("history/" + str(last_version+1), "w")
-new_file.write("abc")
+new_file.write("abc\n")
 new_file.close()
 
 version_file = open("history/last", "w")
-version_file.write(str(last_version+1))
+version_file.write(str(last_version+1)+"\n")
 version_file.close()
 
